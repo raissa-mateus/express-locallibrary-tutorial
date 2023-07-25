7335249fde6744260943f3344e482da60ed505a6
“DasNeve” <henriquemauriciomagnus@gmail.com>
2023-07-25 10:57:25 -0300

    atividade da aula

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,16 @@ const router = express.Router();
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
+  router.get("/cool", function (req, res, next) {
+    res.send("you are so cool");
+});
+
+router.get("/cool/praia", function (req, res, next) {
+  res.send("eu adoro praia");
+});
+
+router.get("/ifc", function (req, res, next) {
+  res.send("Bem vindo ao IFC ");
+});
 
 module.exports = router;
